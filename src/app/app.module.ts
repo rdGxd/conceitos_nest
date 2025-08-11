e587bc04edd6c5bf12fdc30b5b29e8e50edbbbef
Rodrigo <rdg.hora1@gmail.com>
2025-08-11 07:11:50 -0300

    Criando meu próprio módulo (Module) manualmente ou com Nest CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConceitosCliModule } from 'src/conceitos-cli/conceitos-cli.module';
+import { ConceitosManualModule } from 'src/conceitos-manual/conceitos-manual.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConceitosManualModule, ConceitosCliModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [],
 })
 export class AppModule {}
