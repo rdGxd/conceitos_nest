f5f09230678179ed3a0e85cb0880b429624927a1
Rodrigo <rdg.hora1@gmail.com>
2025-08-11 14:22:28 -0300

    Refactor ValidationPipe configuration for improved readability

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // remove chaves que nao estão no DTO
-    transform: false, // tenta transformar os tipos de dados de PARAM e DTOs
-    forbidNonWhitelisted: true, // retorna erro se a chave não estiver no DTO
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //* remove chaves que nao estão no DTO
+      //* TENTE USAR O ParseIntPipe ou algo do tipo direto no controller
+      transform: false, //* tenta transformar os tipos de dados de PARAM e DTOs
+      forbidNonWhitelisted: true, //* retorna erro se a chave não estiver no DTO
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
