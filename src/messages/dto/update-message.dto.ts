8417b6e4760b74daf749075e54153423983a8db4
Rodrigo <rdg.hora1@gmail.com>
2025-08-11 14:00:27 -0300

    Use PartialType de Mapped-Types para a validação de campos em UpdateRecadoDto

--- src/messages/dto/update-message.dto.ts
+++ src/messages/dto/update-message.dto.ts
@@ -1,8 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsBoolean, IsOptional } from 'class-validator';
 import { CreateMessageDto } from './create-message.dto';
 
 export class UpdateMessageDto extends PartialType(CreateMessageDto) {
-  readonly text?: string;
-  readonly sender?: string;
-  readonly to?: string;
+  @IsBoolean()
+  @IsOptional()
+  readonly isRead?: boolean;
 }
