0a0c540e7b8b5a33dd2bb1105591105c0cd716d5
Rodrigo <rdg.hora1@gmail.com>
2025-08-11 13:49:55 -0300

    Validando dados de entrada com useGlobalPipes, ValidationPipe e class-validator

--- src/messages/dto/create-message.dto.ts
+++ src/messages/dto/create-message.dto.ts
@@ -1,5 +1,21 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateMessageDto {
+  @IsString({ message: 'Text must be a string' })
+  @IsNotEmpty({ message: 'Text is required' })
+  @MinLength(5, { message: 'Text must be at least 5 characters long' })
+  @MaxLength(255, { message: 'Text must be at most 255 characters long' })
   readonly text: string;
+
+  @IsString({ message: 'Sender must be a string' })
+  @IsNotEmpty({ message: 'Sender is required' })
+  @MinLength(3, { message: 'Sender must be at least 5 characters long' })
+  @MaxLength(50, { message: 'Sender must be at most 50 characters long' })
   readonly sender: string;
+
+  @IsString({ message: 'Recipient must be a string' })
+  @IsNotEmpty({ message: 'Recipient is required' })
+  @MinLength(3, { message: 'Recipient must be at least 5 characters long' })
+  @MaxLength(50, { message: 'Recipient must be at most 50 characters long' })
   readonly to: string;
 }
