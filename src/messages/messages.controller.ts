546406cc4fd410407e6f654d5d900ef1224a092b
Rodrigo <rdg.hora1@gmail.com>
2025-08-14 14:11:21 -0300

    Remove módulo dinâmico e refatoração do controlador de mensagens

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,35 +1,25 @@
-import
-  {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Inject,
-    Param,
-    Patch,
-    Post,
-    Query,
-    UseGuards,
-    UsePipes,
-  } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { IsAdminGuard } from 'src/common/guards/is-admin.guard';
 import { ParseStringUUIDPipe } from 'src/common/pipes/parse-string-uuid.pipe';
-
-import { MY_DYNAMIC_MODULE_CONFIG, type MyDynamicModuleConfigs } from 'src/my-dynamic/my-dynamic.module';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  constructor(
-    private readonly messagesService: MessagesService,
-    @Inject(MY_DYNAMIC_MODULE_CONFIG)
-    private readonly myDynamicModuleConfig: MyDynamicModuleConfigs,
-  ) {
-    console.log('Dynamic Module Config:', this.myDynamicModuleConfig);
-  }
+  constructor(private readonly messagesService: MessagesService) {}
 
   @Post()
   create(@Body() createMessageDto: CreateMessageDto) {
--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MyDynamicModule } from 'src/my-dynamic/my-dynamic.module';
-import { UsersModule } from 'src/users';
 import { Message } from './entities/message.entity';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
+import { UsersModule } from 'src/users';
 
 @Module({
   controllers: [MessagesController],
@@ -12,13 +11,6 @@ import { MessagesService } from './messages.service';
   // Exporta o MessagesService para que possa ser usado em outros módulos
   exports: [MessagesService],
   // Importa o módulo TypeOrmModule com a entidade Message para criar no banco de dados e o UsersModule para acessar os usuários
-  imports: [
-    TypeOrmModule.forFeature([Message]),
-    UsersModule,
-    MyDynamicModule.register({
-      apiKey: 'my-api-key',
-      apiUrl: 'https://my-api-url.com',
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([Message]), UsersModule],
 })
 export class MessagesModule {}
